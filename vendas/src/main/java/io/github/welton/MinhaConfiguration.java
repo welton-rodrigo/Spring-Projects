eb5581dcf81c0b9e22c0a3e7c505f8e8f9d1d747
Weltonjc1 <weltonjc1@hotmail.com>
2024-06-13 14:29:13 -0300

    commit inicial

---
+++ vendas/src/main/java/io/github/welton/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.welton;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class MinhaConfiguration {
+
+
+
+}
