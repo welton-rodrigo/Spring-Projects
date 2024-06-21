ba25fc198fa731dc1d0f77651baae2c586c4fe68
Weltonjc1 <weltonjc1@hotmail.com>
2024-06-21 17:23:49 -0300

    fix method deleteByNome, add @Transaction

--- vendas/src/main/java/io/github/welton/domain/repositorio/Clientes.java
+++ vendas/src/main/java/io/github/welton/domain/repositorio/Clientes.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public interface Clientes extends JpaRepository<Cliente, Integer> {
 
     List<Cliente> findByNomeLike(String nome);
 
+    @Transactional
     @Modifying
     void deleteByNome(String nome);
 
