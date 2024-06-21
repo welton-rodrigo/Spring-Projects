894267986d7e0318288bece2f94c6eacb456089d
Weltonjc1 <weltonjc1@hotmail.com>
2024-06-21 17:14:34 -0300

    27. working with @Query

--- vendas/src/main/java/io/github/welton/VendasApplication.java
+++ vendas/src/main/java/io/github/welton/VendasApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.util.List;
-
 
 @SpringBootApplication
 
@@ -21,11 +19,23 @@ public class VendasApplication {
             System.out.println("Salvando Clientes");
             clientes.save(new Cliente("Raquel"));
             clientes.save(new Cliente("Welton"));
-            clientes.save(new Cliente("gabriel"));
+            clientes.save(new Cliente("Gabriel"));
+
 
             boolean existe = clientes.existsByNome("Welton");
-            System.out.println("existe um cliente com nome Welton?" + existe);
+            System.out.println("Existe um Cliente com nome Welton?" + existe);
 
+            System.out.println("Pesquisando por Welton no Banco?");
+            List<Cliente> lista =  clientes.encontrarPorNome("Welton");
+            lista.forEach(System.out::println);
+
+           System.out.println("deletando por nome");
+           clientes.deleteByNome("Welton");
+
+            System.out.println("listar todos");
+            clientes.findAll();
+            List<Cliente> lista2 = clientes.findAll();
+            lista2.forEach(System.out::println);
         };
     }
 
--- vendas/src/main/java/io/github/welton/domain/repositorio/Clientes.java
+++ vendas/src/main/java/io/github/welton/domain/repositorio/Clientes.java
@@ -1,12 +1,21 @@
 package io.github.welton.domain.repositorio;
 import io.github.welton.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
+    @Query(value = "select c from Cliente c where c.nome like :nome")
+    List<Cliente> encontrarPorNome(@Param("nome") String nome);
+
     List<Cliente> findByNomeLike(String nome);
+
+    @Modifying
+    void deleteByNome(String nome);
 
     boolean existsByNome (String nome);
 }
