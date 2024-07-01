6d33de600154e1af386264c9d3317805dffcdec1
Weltonjc1 <weltonjc1@hotmail.com>
2024-07-01 16:59:46 -0300

    created clienteController method listClientes

--- vendas/src/main/java/io/github/welton/domain/entity/Cliente.java
+++ vendas/src/main/java/io/github/welton/domain/entity/Cliente.java
@@ -18,6 +18,9 @@ public class Cliente {
     @Column(name= "nome")
     private String nome;
 
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
@@ -56,6 +59,14 @@ public class Cliente {
 
     public void setPedidos(Set<Pedido> pedidos) {
         this.pedidos = pedidos;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     @Override
--- vendas/src/main/java/io/github/welton/rest/controller/ClientController.java
+++ vendas/src/main/java/io/github/welton/rest/controller/ClientController.java
@@ -2,10 +2,13 @@ package io.github.welton.rest.controller;
 
 import io.github.welton.domain.entity.Cliente;
 import io.github.welton.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -62,6 +65,17 @@ public class ClientController {
                     }).orElseGet( () -> ResponseEntity.notFound().build() );
     }
 
+    @GetMapping
+    public ResponseEntity find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
+
+    }
 
 }
 
@@ -72,5 +86,6 @@ ANOTAÇÕES.
 Optional -> pode ou nao existir por isso retorna um optional
 @RequestBody os dados que vem da requisição
 @putMapping - > para atualizar integralmente
+Example
 
 * */
