91592c4d2a922a3eb2d80dda1077de4af561b0c1
Weltonjc1 <weltonjc1@hotmail.com>
2024-07-04 11:06:39 -0300

    ProdutoController

--- vendas/src/main/java/io/github/welton/rest/controller/ProdutoController.java
+++ vendas/src/main/java/io/github/welton/rest/controller/ProdutoController.java
@@ -1,9 +1,15 @@
 package io.github.welton.rest.controller;
 
+import io.github.welton.domain.entity.Cliente;
 import io.github.welton.domain.entity.Produto;
 import io.github.welton.domain.repository.Produtos;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -20,5 +26,35 @@ public class ProdutoController {
     public Produto save(@RequestBody Produto produto){
         return produtos.save(produto);
     }
+
+    @GetMapping
+    public List<Cliente> find (Produto produto){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(produto, matcher);
+        return produtos.findAll(example);
+    }
+
+    @GetMapping("{id}")
+    //um metodo que retorna um cliente e recebe um paramentro id
+    public Produto findProdutoById(@PathVariable Integer id){
+        return produtos.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "CLIENTE NÃO ENCONTRADO"));
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        produtos.findById(id)
+                .map( produto -> {
+                    produtos.delete(produto);
+                    return produto;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "PRODUTO NÃO ENCONTRADO"));
+    }
+
 
 }
